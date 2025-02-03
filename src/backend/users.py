01a7672757950f0b6b3b9040af2f57b4240f60f3
LatteMiguelangel <leonet.miguelangel.2021@gmail.com>
2025-02-02 21:18:22 -0400

    function: generate_user_id

--- src/backend/users.py
+++ src/backend/users.py
@@ -1,8 +1,17 @@
+import random
+import string
 
 # Datos de prueba para comprobar si la validacion funcionaba (ideal para pruebas)
 email_list = ["sant@gmail.com", "acness@hotmail.com", "sidness@example.com"]
 user_list = ["sant04", "acness08", "sidness26"]
+user_id_list = ["VE0001ABC", "VE0002DEF", "VE0003GHI"]
 password_list = ["12345", "67890", "98765"]
+
+# Funcion que genera el id de usuario
+def generate_user_id(pais="VE"):
+    nums = ''.join((random.choices(string.ascii_uppercase, k=4)))
+    ch = ''.join((random.choices(string.digits, k=4)))
+    return f"{pais}{nums}{ch}"
 
 
 # Funcion que guarda los datos en las listas, es llamada en el register
@@ -11,7 +20,7 @@ def save_data(email, user, password):
     email_list.append(email)
     user_list.append(user)
     password_list.append(password)
-
+    user_id_list.append(generate_user_id())
     print(email_list)
     print(user_list)
     print(password_list)
