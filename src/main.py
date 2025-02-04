029266a1786259bd59561ba98a6c61ef927cd8e8
LatteMiguelangel <leonet.miguelangel.2021@gmail.com>
2025-02-04 17:56:18 -0400

    refactor: main.py. requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import flet as ft
+import asyncio
+from backend.async_server import start_async_server
 from views.register import register
-
 
 def main(page: ft.Page):
     page.title = "Glind"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.bgcolor = ft.Colors.TRANSPARENT
+    # Iniciar el servidor asíncrono
+    asyncio.create_task(start_async_server())
     register(page)
 
 
